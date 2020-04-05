7b2626cb88da3f8d56983ceab793d7f8385a18cd
Brian Bamsch <bbamsch@google.com>
2020-04-05 14:32:55 -0700

    Add riscv64 to LP64 / CLANG / LLD archs

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -15,16 +15,17 @@ SKEY?=		yes
 # Set `YP' to `yes' to build with support for NIS/YP.
 YP?=		yes
 
-CLANG_ARCH=aarch64 amd64 arm i386 mips64 mips64el powerpc powerpc64 sparc64
+CLANG_ARCH=aarch64 amd64 arm i386 mips64 mips64el powerpc powerpc64 riscv64 sparc64
 GCC4_ARCH=alpha hppa sh sparc64
 GCC3_ARCH=m88k
-LLD_ARCH=aarch64 amd64 arm i386 powerpc64
+LLD_ARCH=aarch64 amd64 arm i386 powerpc64 riscv64
 
 .if ${MACHINE} == "sgi"
 GCC4_ARCH+=mips64
 .endif
 
 # m88k: ?
+# riscv64: ?
 PIE_ARCH=aarch64 alpha amd64 arm hppa i386 mips64 mips64el powerpc powerpc64 sh sparc64
 STATICPIE_ARCH=aarch64 alpha amd64 arm hppa i386 mips64 mips64el powerpc powerpc64 sh sparc64
 
