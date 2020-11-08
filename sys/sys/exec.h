5cfceb8918bb9cf64edc616ae436c22616a2d821
Dale Rahn <drahn@dalerahn.com>
2020-11-08 15:42:48 -0800

    OpenBSD RISC-V port from mlarkin/
    other contributors:
    Brian Bamsch <bbamsch@google.com>
    Wenyan He <wenyan.he@sjsu.edu>
    Mars Li <mengshi.li.mars@gmail.com>
    Shivam Waghela <shivamwaghela@gmail.com>

--- sys/sys/exec.h
+++ sys/sys/exec.h
@@ -289,6 +289,7 @@ struct exec {
 #define	MID_MIPS64	158	/* big-endian MIPS64 */
 #define	MID_ARM64	159	/* ARM64 */
 #define	MID_POWERPC64	160	/* big-endian 64-bit PowerPC */
+#define	MID_RISCV64	161	/* Little-endian 64-bit RISC-V */
 #define	MID_HP200	200	/* hp200 (68010) BSD binary */
 #define	MID_HP300	300	/* hp300 (68020+68881) BSD binary */
 #define	MID_HPUX	0x20C	/* hp200/300 HP-UX binary */
